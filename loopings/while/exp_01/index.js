bc8a657b0ed74f5623449b9d64d78b9693c8e490
eurico77 <euricomagalhaes61@gmail.com>
2022-01-31 23:25:32 -0300

    feat: add exp of while"

---
+++ loopings/while/exp_01/index.js
@@ -0,0 +1,9 @@
+const constelation = 'Andromeda';
+let position = 0;
+let constalationLength = constelation.length;
+
+while (position < constalationLength) {
+  constelation[position] === 'a' || constelation[position] === 'A' ? console.log(position) : null
+  position++;
+}
+
